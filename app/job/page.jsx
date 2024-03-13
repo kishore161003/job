853819e7761d08448b65535aa00a793b30e26b4e
kishore161003 <kishorekumarapandianp21cs@psnacet.edu.in>
2024-03-13 20:02:59 +0530

    update

--- app/job/page.jsx
+++ app/job/page.jsx
@@ -15,7 +15,7 @@ const Page = ({ isChanged = false }) => {
   const fetchJobData = async (search = "") => {
     console.log("searchtext", search);
     if (search.length == 0) {
-      fetch("/api/job")
+      await fetch("/api/job")
         .then((res) => res.json())
         .then((data) => {
           setJobs(data);
